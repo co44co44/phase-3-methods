1255ca99bf516e1670094e86145f31c56ce5e038
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-03-30 01:22:05 -0400

    commit tree 84223c1ad9c0c2b3b272673b404170a02e31e779

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+  end
+
+  my_method_return_value = my_method(1)
+
+ def greet_programmer() 
+   puts "Hello, programmer!"
+ end
+
+ def greet name
+    puts "Hello, #{name}!"
+ end
+
+ def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+ end
+
+ def add num1, num2
+    num1 + num2
+ end
+
+ def halve number
+    return nil unless number.class == Integer
+    number /2
+ end
